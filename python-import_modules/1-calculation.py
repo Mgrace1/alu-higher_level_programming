88f6488aa3e7aecce56cc9e027978d98514cf5dc
Mgrace1 <g.munezero1@alustudent.com>
2023-11-02 01:48:53 -0700

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,1 +1,10 @@
-__import__("0-add")
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
