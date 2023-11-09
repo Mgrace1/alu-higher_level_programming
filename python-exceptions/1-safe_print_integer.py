d91c0f6ff50fed1c216579e3f0dab638643a2d0b
Mgrace1 <g.munezero1@alustudent.com>
2023-11-09 02:02:40 -0800

    1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    num_returned = 0
-    for i in range(x):
-        try:
-            print(f"{my_list[i]}", end="")
-            num_returned += 1
-        except IndexError as IE:
-            break
-    print()
-    return num_returned
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
