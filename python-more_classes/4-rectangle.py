bba977c9f92d399c7dbf6abbc8827abf698f8b4e
Mgrace1 <g.munezero1@alustudent.com>
2023-11-13 01:34:12 -0800

    4-rectangle.py

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -73,6 +73,7 @@ class Rectangle:
                 rect.append("\n")
         return ("".join(rect))
 
+
     def __repr__(self):
         """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
