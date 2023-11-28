3b588de1a9d69cbd41ba9f35d6a783693dbdf051
Mgrace1 <g.munezero1@alustudent.com>
2023-11-28 09:51:52 -0800

    12-no_cheating.sql

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
--- List cordinates according to a particular cond.
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+-- Update the row in a table.
+UPDATE second_table SET score = 10 WHERE name = "Bob";
