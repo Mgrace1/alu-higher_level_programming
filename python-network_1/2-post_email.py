e96a68b5d65ef3d81c0792760cf9c3d833472253
root <Mgrace1>
2023-12-01 12:48:51 +0000

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-""" A script that sends POST request
 """
+Script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body response
+"""
+from sys import argv
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 
 
-if __name__ == '__main__':
-    from urllib.request import urlopen, Request
-    from urllib.parse import urlencode
-    import sys
-    data = urlencode({"email": sys.argv[2]
-                      }).encode("ascii")
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = urlencode(value).encode("ascii")
+    req = Request(url, data)
 
-    request = Request(sys.argv[1], data)
-    with urlopen(request) as response:
-        print(response.read().decode('utf-8'))
-  
+    with urlopen(req) as response:
+        print(response.read().decode("utf-8", "replace"))
